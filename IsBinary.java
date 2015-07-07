d6c3902437990dcd729a2210f2a6135c9c3d9896
Faïza Harbi <faiza.harbi@gmail.com>
2015-07-07 11:46:24 +0200

    Contains the Main of the program
    Small Main to go with the actual program

---
+++ IsBinary.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @author faiza harbi
+ * @since 07/05/2015
+ * @version 1.3
+ * 
+ * 
+ * Take a string as an input, change it to an int and check if the int is
+ * binary or not.
+ * Returns "is Binary" if so, "is not Binary" otherwise.
+ * 
+ * 
+ */
+ 
+
+public class IsBinary {
+
+	@SuppressWarnings("unused")
+	private static checkbinary binary;
+
+	public static void main(String[] args) throws IOException {
+		
+	    binary = new checkbinary();
+	    
+		
+		
+
+	}
+
+}
